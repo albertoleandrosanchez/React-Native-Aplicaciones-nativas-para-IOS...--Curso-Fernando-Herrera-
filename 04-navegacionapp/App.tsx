276357a62964761b0f7f0118da13a6091b985d51
albertoleandrosanchez <albertoleandrosanchez@gmail.com>
2022-06-09 11:08:45 -0300

    react navigator

--- 04-navegacionapp/App.tsx
+++ 04-navegacionapp/App.tsx
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 // import { StackNavigator } from './src/navigator/StackNavigator';
 // import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
-import {MenuLateral} from './src/navigator/MenuLateral';
-import {Tabs} from './src/navigator/Tabs';
+import { MenuLateral } from './src/navigator/MenuLateral';
+
 
 const App = () => {
   return (
@@ -13,9 +13,8 @@ const App = () => {
       {/* <StackNavigator /> */}
       {/* <MenuLateralBasico /> */}
       <MenuLateral />
-      {/* <Tabs /> */}
     </NavigationContainer>
-  );
-};
+  )
+}
 
 export default App;
--- 04-navegacionapp/src/navigator/MenuLateral.tsx
+++ 04-navegacionapp/src/navigator/MenuLateral.tsx
@@ -1,68 +1,67 @@
 import React from 'react';
 
-import {
-  createDrawerNavigator,
-  DrawerContentComponentProps,
-  DrawerContentOptions,
-  DrawerContentScrollView,
-} from '@react-navigation/drawer';
+import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentOptions, DrawerContentScrollView } from '@react-navigation/drawer';
 
-import {StackNavigator} from './StackNavigator';
-import {SettingsScreen} from '../screens/SettingsScreen';
-import {
-  Image,
-  Text,
-  useWindowDimensions,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import {styles} from '../theme/appTheme';
-import {Tabs} from './Tabs';
+import { StackNavigator } from './StackNavigator';
+import { SettingsScreen } from '../screens/SettingsScreen';
+import { Image, Text, useWindowDimensions, View, TouchableOpacity } from 'react-native';
+import { styles } from '../theme/appTheme';
+
 
 const Drawer = createDrawerNavigator();
 
-export const MenuLateral = () => {
-  const {width} = useWindowDimensions();
 
-  return (
+
+export const MenuLateral = () => {
+  
+    const { width } = useWindowDimensions();
+
+    return (
     <Drawer.Navigator
-      drawerType={width >= 768 ? 'permanent' : 'front'}
-      drawerContent={(props) => <MenuInterno {...props} />}>
-      <Drawer.Screen name="Tabs" component={Tabs} />
-      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+      drawerType={ width >= 768 ? 'permanent' : 'front' }
+      drawerContent={ (props) => <MenuInterno { ...props } /> }
+    >
+      <Drawer.Screen name="StackNavigator" component={ StackNavigator } />
+      <Drawer.Screen name="SettingsScreen" component={ SettingsScreen } />
     </Drawer.Navigator>
   );
-};
+}
 
-const MenuInterno = ({
-  navigation,
-}: DrawerContentComponentProps<DrawerContentOptions>) => {
+const MenuInterno = ( { navigation }: DrawerContentComponentProps<DrawerContentOptions>) => {
+
   return (
     <DrawerContentScrollView>
+
       {/* Parte del avatar */}
-      <View style={styles.avatarContainer}>
-        <Image
+      <View style={ styles.avatarContainer }>
+        <Image 
           source={{
-            uri: 'https://medgoldresources.com/wp-content/uploads/2018/02/avatar-placeholder.gif',
+            uri: 'https://medgoldresources.com/wp-content/uploads/2018/02/avatar-placeholder.gif'
           }}
-          style={styles.avatar}
+          style={ styles.avatar }
         />
       </View>
 
-      {/* Opciones de menú */}
-      <View style={styles.menuContainer}>
-        <TouchableOpacity
-          style={styles.menuBoton}
-          onPress={() => navigation.navigate('Tabs')}>
-          <Text style={styles.menuTexto}>Navegacion</Text>
-        </TouchableOpacity>
 
-        <TouchableOpacity
-          style={styles.menuBoton}
-          onPress={() => navigation.navigate('SettingsScreen')}>
-          <Text style={styles.menuTexto}>Ajustes</Text>
-        </TouchableOpacity>
+      {/* Opciones de menú */}
+      <View style={ styles.menuContainer }>
+
+          <TouchableOpacity 
+            style={ styles.menuBoton }
+            onPress={ () => navigation.navigate('StackNavigator') }
+          >
+            <Text style={ styles.menuTexto }>Navegacion</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity 
+            style={ styles.menuBoton }
+            onPress={ () => navigation.navigate('SettingsScreen') }
+          >
+            <Text style={ styles.menuTexto }>Ajustes</Text>
+          </TouchableOpacity>
+
       </View>
+
     </DrawerContentScrollView>
   );
-};
+}

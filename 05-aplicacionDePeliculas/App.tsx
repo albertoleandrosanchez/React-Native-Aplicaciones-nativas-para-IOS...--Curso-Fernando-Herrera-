603c32c146c0fd49dabb41a01cfdc6586eeb1260
albertoleandrosanchez <albertoleandrosanchez@gmail.com>
2022-06-09 12:54:36 -0300

    cap 150

--- 05-aplicacionDePeliculas/App.tsx
+++ 05-aplicacionDePeliculas/App.tsx
@@ -1,11 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {Text} from 'react-native';
+import {Navigation} from './src/navigation/NavigationScreen';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Text>Hello</Text>
+      <Navigation />
     </NavigationContainer>
   );
 }
